d6bd8c6f2951994be8f8e55c82534bd9c753667f
jvitor22 <j.vitor.a@gmail.com>
2023-06-10 15:09:39 -0300

    adjustment in the request header

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -15,7 +15,7 @@ function AuthProvider({ children }) {
       localStorage.setItem('@food_explorer:user', JSON.stringify(user))
       localStorage.setItem('@food_explorer:token', token)
 
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
       setData({ user, token })
     } catch (error) {
       if (error.response) {
@@ -38,7 +38,7 @@ function AuthProvider({ children }) {
     const user = localStorage.getItem('@food_explorer:user')
 
     if (token && user) {
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
       setData({
         token,
